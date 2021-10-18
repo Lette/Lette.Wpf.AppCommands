5ac3025b3aeaefc142161b04d7d5b7319a80c425
Christoffer Lette <christoffer.lette@gmail.com>
2021-10-18 14:11:04 +0200

    Produces separate symbol package.

--- pack.ps1
+++ pack.ps1
@@ -1,4 +1,7 @@
+rm *.nupkg
+rm *.snupkg
 dotnet restore
 dotnet build --no-restore --no-incremental -c Release /p:ContinuousIntegrationBuild=true
 dotnet pack .\Lette.Wpf.AppCommands --no-build -c Release
 mv .\Lette.Wpf.AppCommands\bin\Release\*.nupkg .
+mv .\Lette.Wpf.AppCommands\bin\Release\*.snupkg .
