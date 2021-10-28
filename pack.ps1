c9989be747dc627f038d6b228196182800a390d6
Christoffer Lette <christoffer.lette@gmail.com>
2021-10-29 00:02:27 +0200

    Updated build script.

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,13 @@
+$ErrorActionPreference = "Stop"
+
 rm *.nupkg
 rm *.snupkg
+
 dotnet restore
-dotnet build --no-restore --no-incremental -c Release /p:ContinuousIntegrationBuild=true
+
+dotnet build --no-restore --no-incremental -c Release -p:ContinuousIntegrationBuild=true
+
 dotnet pack .\Lette.Wpf.AppCommands --no-build -c Release
+
 mv .\Lette.Wpf.AppCommands\bin\Release\*.nupkg .
 mv .\Lette.Wpf.AppCommands\bin\Release\*.snupkg .
